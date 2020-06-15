8e127857347db9efc95185d003679a4450e1a6cb
Maxwell Lyu <xlworkshop.chs@hotmail.com>
2020-06-15 20:46:16 +0800

    alter: use another implementation

--- source/cg_algorithms.py
+++ source/cg_algorithms.py
@@ -356,6 +356,8 @@ def clip(p_list, x_min, y_min, x_max, y_max, algorithm):
                         return [(round(x1), round(y1)), (round(x2), round(y2))]
         pass
     elif algorithm == 'Sutherland-Hodgeman':
+        return clip_sh(p_list, [[x_max, y_min], [x_max, y_max], [x_min, y_max], [x_min, y_min], ])
+        '''
         vsold = p_list
         vsnew = []
         # 对左边界进行操作
@@ -450,5 +452,39 @@ def clip(p_list, x_min, y_min, x_max, y_max, algorithm):
                     vsnew.append((vp2[0] + (vp1[0] - vp2[0]) * (y_max - vp2[1]) / (vp1[1] - vp2[1]), y_max))
             vp1 = vp2
         return vsnew
+        '''
     return []
     
+
+def clip_sh(subjectPolygon, clipPolygon):
+   def inside(p):
+      return(cp2[0]-cp1[0])*(p[1]-cp1[1]) >= (cp2[1]-cp1[1])*(p[0]-cp1[0])
+ 
+   def computeIntersection():
+      dc = [ cp1[0] - cp2[0], cp1[1] - cp2[1] ]
+      dp = [ s[0] - e[0], s[1] - e[1] ]
+      n1 = cp1[0] * cp2[1] - cp1[1] * cp2[0]
+      n2 = s[0] * e[1] - s[1] * e[0] 
+      n3 = 1.0 / (dc[0] * dp[1] - dc[1] * dp[0])
+      return [(n1*dp[0] - n2*dc[0]) * n3, (n1*dp[1] - n2*dc[1]) * n3]
+ 
+   outputList = subjectPolygon
+   cp1 = clipPolygon[-1]
+ 
+   for clipVertex in clipPolygon:
+      cp2 = clipVertex
+      inputList = outputList
+      outputList = []
+      s = inputList[-1]
+ 
+      for subjectVertex in inputList:
+         e = subjectVertex
+         if inside(e):
+            if not inside(s):
+               outputList.append(computeIntersection())
+            outputList.append(e)
+         elif inside(s):
+            outputList.append(computeIntersection())
+         s = e
+      cp1 = cp2
+   return(outputList)
